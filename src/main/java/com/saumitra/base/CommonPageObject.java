21953ee77be10042fd8fb8e1f1dfa2baf7c06370
Saumitra Kulkarni <sakulkarni96@gmail.com>
2025-11-27 22:15:04 +0530

    Fix the failure with GetUtility attempt - 1

--- src/main/java/com/saumitra/base/CommonPageObject.java
+++ src/main/java/com/saumitra/base/CommonPageObject.java
@@ -1,17 +1,22 @@
 package com.saumitra.base;
 
+import com.saumitra.utlis.GetUtility;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 
 public class CommonPageObject {
 
     // Make it protected, not static - each test should have its own driver
     protected WebDriver driver;
+    protected GetUtility utils;
 
     // Constructor to set driver
     public CommonPageObject(WebDriver driver) {
         this.driver = driver;
+        this.utils = new GetUtility(driver);
+        PageFactory.initElements(driver, this);
     }
 
     // Instance methods (not static)
